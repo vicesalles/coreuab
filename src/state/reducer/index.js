1ba8e5791f4d28888ded7fc81aeed91b23523fdf
vicesallesMac <vicenc.salles@gmail.com>
2018-01-10 08:53:42 +0100

    kick off

---
+++ src/state/reducer/index.js
@@ -0,0 +1,64 @@
+const initialState = {
+
+    chapters:[
+        {
+            title:'Seu de La Draga',
+            description:"La seu del Campus d’Arqueologia UAB de La Draga, es troba situada a Banyoles, Pla de l’Estany, Girona. Inaugurada al novembre del 2014 és el resultat de més de 15 anys d’estreta col·laboració entre el Grup de Recerca AGREST, l'Institució Milà i Fontanals, el Museu d'Arqueologia de Catalunya i l’Ajuntament de Banyoles. El parc neolític de la Draga és una extensió del Museu Arqueològic Comarcal de Banyoles, institució que s’encarrega de la seva gestió i coordinació. La docència, la recerca i els treballs arqueològics són duts a terme pel Consell Superior d’Investigacions Científiques, la Universitat Autònoma de Barcelona i el Museu d’Arqueologia de Catalunya. ",
+            vidId:'XqBICtjKcA0',
+            url:'http://www.uab.cat/web/investigar/campus-d-arqueologia-i-paleontologia-uab/seu-de-la-draga/la-seu-de-la-draga-1345697834059.html',
+            localitzacio:'https://goo.gl/maps/6ScS8nAipZo',
+            fotoRoute:'draga',
+            institutions:[
+                {
+                    name:'Grup de Recerca AGREST',
+                    logo:'',
+                    url:'http://grupsderecerca.uab.cat/agrest/'
+                },{
+                    name:"Institució Milà i Fontanals",
+                    logo:'',
+                    url:'http://www.imf.csic.es/'
+                },{
+                    name:"Museu d'Arqueologia de Catalunya",
+                    logo:'',
+                    url:"http://www.mac.cat/"
+                },{
+                    name:"Ajuntament de Banyoles",
+                    logo:'',
+                    url:"http://www.banyoles.cat/"
+                },{
+                    name:'Museu Arqueològic Comarcal de Banyoles',
+                    logo:'',
+                    url:'http://www.museusdebanyoles.cat/arqueologic/'
+                },{
+                    name:'Universitat Autònoma de Barcelona',
+                    logo:'',
+                    url:'http://www.uab.cat/'
+                }
+            ],
+            people:[
+                {
+                    name:"Antoni Palomo",
+                    title:"Professor del Departament de Prehistòria de la UAB",
+                    institution:"Arqueolític",
+                    linkedin:"https://es.linkedin.com/in/antoni-palomo-p%C3%A9rez-89867290",
+                    url:"http://www.uab.cat/web/qui-som/antoni-palomo-perez/projectes-1345649074266.html",
+                    academia:"http://uab.academia.edu/AntoniPalomo"
+                },{
+                    name:"Lluís Figueras",
+                    title:"Director del Museu Arqueològic Comarcal de Banyoles",
+                    institution:"",
+                    linkedin:"",
+                    url:""
+                },{
+                    name:"Maria Saña",
+                    title:"Professora del Departament de Prehistòria de la UAB",
+                    institution:"",
+                    linkedin:"https://www.linkedin.com/in/maria-sa%C3%B1a-segu%C3%AD-45807a60/",
+                    url:"http://www.uab.cat/web/el-departament/Sana-1345649043596.html",
+                    academia:"http://uab.academia.edu/MariaSana"
+                }
+            ]
+        }
+    ]
+
+}
